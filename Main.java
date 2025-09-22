a570b69e42df6b43ceb5da60c3a8a60edfea0a14
kunruethaipat <kunruethai.pat@gmail.com>
2025-09-22 21:49:06 +0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        // create object
+        Vehicle vehicle = new Vehicle("SN11101", "Honda", "Civic", 2020);
+
+        // print info
+        System.out.println("=== Vehicle Information ===");
+        System.out.println(vehicle);
+    }
+}
